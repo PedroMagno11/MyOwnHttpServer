5f4d7d99eb2a7209aabac2ec7642bf9ba12b7adc
Pedro Magno <pedromagnopro@gmail.com>
2025-05-12 22:16:09 -0300

    fix: adiciona encode StandardCharsets.UTF_8 ao getBytes()

--- src/main/java/br/com/pedromagno/adapter/ResponseWriter.java
+++ src/main/java/br/com/pedromagno/adapter/ResponseWriter.java
@@ -4,17 +4,18 @@ import br.com.pedromagno.domain.HttpResponse;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class ResponseWriter {
     public static void write(HttpResponse response, OutputStream outputFromServer) throws IOException {
-        outputFromServer.write(("HTTP/1.1" + response.getStatusCode() + " " + response.getReasonPhrase() + "\r\n").getBytes());
+        outputFromServer.write(("HTTP/1.1 " + response.getStatusCode() + " " + response.getReasonPhrase() + "\r\n").getBytes(StandardCharsets.UTF_8));
 
         for (Map.Entry<String, String> header : response.getHeaders().entrySet()){
-            outputFromServer.write((header.getKey() + ": " + header.getValue() + "\r\n").getBytes());
+            outputFromServer.write((header.getKey() + ": " + header.getValue() + "\r\n").getBytes(StandardCharsets.UTF_8));
         }
 
-        outputFromServer.write("\r\n".getBytes());
+        outputFromServer.write("\r\n".getBytes(StandardCharsets.UTF_8));
 
         if(response.getBody() != null){
             outputFromServer.write(response.getBody());
